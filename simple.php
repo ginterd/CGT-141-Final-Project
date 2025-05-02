5b2d2666c42611576a04caa5b6c36d7d644e02ed
ginterd <ginterd@purdue.edu>
2025-05-02 18:51:34 -0400

    Add files via upload

--- simple.php
+++ simple.php
@@ -14,3 +14,16 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     echo "Thanks, your message has been received!";
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>You did it!</p>
+    
+</body>
+</html>

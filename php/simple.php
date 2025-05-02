5ecb9222aa0487ce945dc95714f3bb31482619ae
ginterd <ginterd@purdue.edu>
2025-05-02 18:35:38 -0400

    Add files via upload

--- php/simple.php
+++ php/simple.php
@@ -1,7 +1,14 @@
 <?php
-  if (window.location.search.includes('success=true')) {
-    alert("Your message his been sent. Far out!");
-  }
+// Server-side: simple.php
+
+// Capture the form fields (optional: add validation/sanitization)
+$email = $_POST['email'] ?? '';
+$query = $_POST['query'] ?? '';
+$message = $_POST['message'] ?? '';
+
+// You could store this in a database, email it, etc.
+// For now, we just redirect.
+
+header("Location: ../index.html?success=true"); // Go back to the HTML page
+exit();
 ?>
-
-
--- script.js
+++ script.js
@@ -16,6 +16,10 @@
   }
 
 
+  if (window.location.search.includes('success=true')) {
+    alert("Your message has been sent. Far out!");
+  }
+
   /*References
   https://www.w3schools.com/jsref/prop_style_display.asp
   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
